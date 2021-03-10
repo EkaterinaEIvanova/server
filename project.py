5c278ebfb9072718fa45cb90dc567604be5798e2
Ekaterina <you@example.com>
2021-03-11 00:40:32 +0300

    fix errors

--- project.py
+++ project.py
@@ -1,18 +1,15 @@
 from aiohttp import web
 from threading import Lock
 
+
 class Handler:
 
     def __init__(self):
         self.counter = 0
 
-    def safe_increment(self):
-        Lock().acquire()
-        self.counter += 1
-        Lock().release()
-
     async def handle(self, request):
-        self.safe_increment()
+        with Lock():
+            self.counter += 1
         return web.Response(text=f'{self.counter}')
 
 
